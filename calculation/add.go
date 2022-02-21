ab781509916097cb3bfe271d5388bd3c58bea39f
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-02-21 14:36:27 +0700

    membuat funtion penjumahan dan memanggilnya di main dari package yang berbeda

---
+++ calculation/add.go
@@ -0,0 +1,8 @@
+package calculation
+
+// function penjumlahan membutuh kan 2 parameter (2 angka) dengan nilai balikan interger, maka penulisan nya seperti dibawah ini
+// nama function Add ( dengan A besar) bertujuan untuk menjadikan function Add ini bersifat public sehingga bisa diakases oleh file lain dengan package yang berbeda
+func Add(numberOne int, numberTwo int) int {
+	// proses kalkulasi
+	return numberOne + numberTwo
+}
--- main.go
+++ main.go
@@ -8,7 +8,14 @@ package main
 // 2. Library
 // ini custom yang kita inginkan, jadi nama package nya selain main , contoh package keren, package naruto dlsb. package ini ga bisa langsung di execute dari teminal. harus masuk dulu ke program utama
 
-import "fmt"
+// untuk import file dari package yang bebeda ada syntac nya
+// yaitu modul/namapackage
+// modul ini di dapat dari gile go.mod dan go.mod ini di dapat dari hasil inisiasi golang pas awal pertama membua project (go mod init namaProject)
+// dan di porject ini nama module nya adalah myapp
+import (
+	"fmt"
+	"myapp/calculation"
+)
 
 func main() {
 	fmt.Println("Hallo Dunia")
@@ -19,4 +26,8 @@ func main() {
 	// bikin variable yang isi nya function Sentence
 	kalimat := Sentence()
 	fmt.Println(kalimat)
+
+	// variable menampung hasil penjumlahan dari function Add di file add.go
+	jumlah := calculation.Add(4, 90)
+	fmt.Println("Hasil penjumlahan", jumlah)
 }
