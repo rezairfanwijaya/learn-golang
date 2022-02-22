f768faff8f4341888719a90f0e4358e4bac9fd91
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-02-22 13:28:56 +0700

    menggunakan syntax := untuk menulis variable

--- variables/kata.go
+++ variables/kata.go
@@ -5,6 +5,10 @@ import "fmt"
 func Kata() string {
 	// syntax
 	// var namaVariable tipedata = "value"
-	var nama string = "Reza Irfan Wijaya"
+	// var nama string = "Reza Irfan Wijaya"
+
+	// atau bisa juga dengan menulis sperti ini
+	nama := "Reza Irfan Wijaya"
 	return fmt.Sprintf(nama)
+
 }
