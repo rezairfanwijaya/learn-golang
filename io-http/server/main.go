8d84e3c41b22b7ac21316ecf18ffca5594aaf967
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-04-05 13:10:56 +0700

    io-http

---
+++ io-http/server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type user struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type message struct {
+	Data string `json:"message"`
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	// set header
+	w.Header().Set("Content-Type", "application/json")
+
+	// set response
+	if r.Method == "POST" {
+		// kita baca semua data yang dikirim user dari body
+		reqbody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		// bikin varible penampung data user
+		newUser := user{}
+
+		// kita buka json untuk dibaca (unmarshal)->decode
+		json.Unmarshal(reqbody, &newUser)
+		log.Printf("i receive this data : %+v", newUser)
+
+		// bikin variable untuk menampung message
+		var msg message
+		msg.Data = fmt.Sprintf("Hello %s, you are %d years old", newUser.Name, newUser.Age)
+		log.Printf("i send this data : %+v", msg)
+
+		// lalu kita encode ke json
+		resultByte, _ := json.Marshal(msg)
+		w.WriteHeader(http.StatusAccepted)
+		w.Write(resultByte)
+		return
+
+	}
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("server is running on http://localhost:8080")
+}
