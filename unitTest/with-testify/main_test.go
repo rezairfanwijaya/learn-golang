35ce37f09343bcbce96a809ed9b3999018c2fb6c
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-04-11 05:04:02 +0700

    skip test

--- unitTest/with-testify/main_test.go
+++ unitTest/with-testify/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,4 +15,12 @@ func TestSum(t *testing.T) {
 func TestHello(t *testing.T) {
 	res := Hello("Reza")
 	assert.Equal(t, "Hello Reza", res)
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("This test does not run on windows")
+	}
+
+	fmt.Println("Ini testing ")
 }
