92cafe9bd87ed9e75a65a3c55e9f00259fe75ce4
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-04-11 23:47:55 +0700

    client req @188

---
+++ web-api/request-client/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// siapkan struct untuk menampung data
+type Student struct {
+	ID    string
+	Name  string
+	Grade int
+}
+
+// base URL
+var baseURL = "http://localhost:8080"
+
+// function untuk mengirimkan request POST ke server
+func fetchUser() ([]Student, error) {
+	// inisiasi
+	var err error
+	var client = &http.Client{}
+	var data []Student
+
+	// buat request
+	request, err := http.NewRequest("GET", baseURL+"/users", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// lakukan aksi terhadap request yang telah dibikin
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// defer
+	defer response.Body.Close()
+
+	// decode hasil response (body/ data yang ditulis client)
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func main() {
+	data, err := fetchUser()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(data)
+}
