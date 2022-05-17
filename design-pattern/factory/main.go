d0e2908a54db791957befc4712210aa4315b05ad
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-05-17 10:24:44 +0700

    refactoring

--- design-pattern/factory/main.go
+++ design-pattern/factory/main.go
@@ -1,14 +1,9 @@
 package main
 
-import (
-	"myapp/design-pattern/factory"
-)
-
 func main() {
 
 	// kita membuat object dari interface content creator
-	var creator factory.ContentCreator
-	creator = &factory.Eko{}
+	var creator ContentCreator = &RezaIrfan{}
 
 	konten := creator.CreateContent()
 	konten.Play()
